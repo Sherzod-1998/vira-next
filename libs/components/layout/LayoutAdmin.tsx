94f50d063fec9dab9a89afd2760c3f78d937e6fd
Sherzod-1998 <sherzod.oltinsoy@gmail.com>
2025-11-23 09:47:55 +0900

    Admin page fixed

--- libs/components/layout/LayoutAdmin.tsx
+++ libs/components/layout/LayoutAdmin.tsx
@@ -141,9 +141,7 @@ const withAdminLayout = (Component: ComponentType) => {
 						className="aside"
 					>
 						<Toolbar sx={{ flexDirection: 'column', alignItems: 'flexStart' }}>
-							<Stack className={'logo-box'}>
-								<img src={'/img/logo/logoText.svg'} alt={'logo'} />
-							</Stack>
+							
 
 							<Stack
 								className="user"
